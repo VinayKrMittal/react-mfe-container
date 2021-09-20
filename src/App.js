55848b622cac5b2365e3e7c996c539eacb81d82c
vinay.kumar1 <vinay.kumar1@telekom-digital.com>
2021-09-20 10:43:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Home from './routes/Home';
+import GreetingCat from './routes/GreetingCat';
+import Summary from './routes/Summary';
+import Cats  from "./routes/Cats";
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cat/:greeting" component={GreetingCat} />
+          <Route exact path='/summary' component={Summary}/>
+          <Route exact path="/gif" component={Cats} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
